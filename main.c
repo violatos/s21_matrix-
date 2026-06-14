0bba99e111e5feb2d5e5accc5e697e2dd347ca1f
violatos <coffee2006cat@gmail.com>
2026-06-14 19:07:38 +0300

    add: s21_eq_matrix.- its compares two matrix

--- main.c
+++ main.c
@@ -2,38 +2,6 @@
 #include "s21_matrix.h"
 
 int main(){
-
-    s21_matrix_t mat, mat2;
-
-    s21_create_matrix(2, 2, &mat);
-    s21_create_matrix(2, 2, &mat2);
-
-    mat.matrix[0][0] = 1.0;
-    mat.matrix[0][1] = 2.0;
-    mat.matrix[1][0] = 3.0;
-    mat.matrix[1][1] = 4.0;
-
-    mat2.matrix[0][0] = 1.0;
-    mat2.matrix[0][1] = 2.0;
-    mat2.matrix[1][0] = 3.0;
-    mat2.matrix[1][1] = 4.0;
-    
-
-    // printf("%.0f\n",mat2.matrix[0][0]);
-
-
-    int res = s21_eq_matrix(&mat,&mat2);
-
-    printf("res = %d\n", res);
-
-
-
-//................remove..................//
-
-    s21_remove_matrix(&mat);
-    if(mat.matrix == NULL && mat.rows == 0 && mat.columns == 0){
-        printf("memory is free!\n");
-    }
-
+    //Maybe there will be examples later.
     return 0;
 }
--- s21_eq_matrix.c
+++ s21_eq_matrix.c
@@ -4,9 +4,21 @@
 
 int s21_eq_matrix(s21_matrix_t *A, s21_matrix_t *B){
 
-
-    if(A->){
-
+//проверка на существование матрицы 
+    if(A == NULL || B == NULL){
+        return FAILURE;//0
+    }
+    if(A->matrix == NULL || B->matrix == NULL){
+        return  FAILURE;//0
+    }
+    if(A->rows < 1 || A->columns < 1){
+        return FAILURE;//0
+    }
+    if(B->rows < 1 || B->columns < 1){
+        return FAILURE;//0
+    }
+    if(A->rows != B->rows || A->columns != B->columns){
+        return FAILURE;//0
     }
 
     int result;
@@ -14,19 +26,18 @@ int s21_eq_matrix(s21_matrix_t *A, s21_matrix_t *B){
     for(int i = 0; i < A->rows; i++){
         for(int j = 0; j < B->columns; j++){
             if(fabs(A->matrix[i][j] - B->matrix[i][j]) < 1e-6){
-                result = 1;
+                result = SUCCESS;//1
             }else{
-                result = 0;
+                result = FAILURE;//0
                 break;
             }
         }
         if(result == 0){
             break;
         }
-    }
+    }        
 
 
-    return result;
-
+    return result;//0 || 1; 
     
 }
