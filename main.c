d57dbae140acb1ba65c2b94f9f214e5848e6dc89
violatos <coffee2006cat@gmail.com>
2026-06-20 14:13:47 +0300

    add: s21_mult_nomber

--- main.c
+++ main.c
@@ -2,7 +2,19 @@
 #include "s21_matrix.h"
 
 int main(){
-    //Maybe there will be examples later.
+
+    s21_matrix_t mat,result;
+
+    s21_create_matrix(2,2,&mat);
+    mat.matrix[0][0] = 1.0;
+    mat.matrix[0][1] = 2.0;
+    mat.matrix[1][0] = 3.0;
+    mat.matrix[1][1] = 4.0;
+
+    s21_mult_number(&mat,2.0,&result);
+
+    printf("result:%f\n", result.matrix[1][1]);
+
     return 0;
 }
 
--- s21_mult_number.c
+++ s21_mult_number.c
@@ -1,27 +1,27 @@
 #include "s21_matrix.h"
 
-
 int s21_mult_number(s21_matrix_t *A, double number, s21_matrix_t *result){
 
 //проверка на существование матрицы 
     if(A == NULL || result == NULL){
         return INCORRECT_MATRIX;//1
     }
-    if(A->matrix == NULL || B->matrix == NULL){
+    if(A->matrix == NULL ){
         return  INCORRECT_MATRIX;//1
     }
     if(A->rows < 1 || A->columns < 1){
         return INCORRECT_MATRIX;//1
     }
-    if(B->rows < 1 || B->columns < 1){
-        return INCORRECT_MATRIX;//1
+
+    int status = s21_create_matrix(A->rows,A->columns,result);
+    if(status == OK){
+        for(int i = 0; i < A->rows; i++){
+            for(int j = 0;j < A->columns; j++){
+                result->matrix[i][j] = A->matrix[i][j] * number; 
+            }
+        }        
     }
-    if(A->rows != B->rows || A->columns != B->columns){
-        return CALCULATION_ERROR;//2
-    }    
 
-
-
-
+    return status; 
 
 }
