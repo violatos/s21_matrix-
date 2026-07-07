1052d835b0b7735856b2cdb3820ac289f3c2e4a9
violatos <coffee2006cat@gmail.com>
2026-07-07 14:02:05 +0300

    add: s21_mult_matrix.c

--- main.c
+++ main.c
@@ -3,24 +3,28 @@
 
 int main(){
 
-    s21_matrix_t mat2, mat, result;
+    s21_matrix_t mat, mat2, result;
 
-    s21_create_matrix(2,3,&mat);
-    s21_create_matrix(3,2,&mat2);
+    s21_create_matrix(3,2,&mat);
+    s21_create_matrix(2,3,&mat2);
 
     mat.matrix[0][0] = 1.0;
-    mat.matrix[0][1] = 2.0;
-    mat.matrix[1][0] = 3.0;
-    mat.matrix[1][1] = 4.0;
+    mat.matrix[0][1] = 4.0;
+    mat.matrix[1][0] = 2.0;
+    mat.matrix[1][1] = 5.0;
+    mat.matrix[2][0] = 3.0;
+    mat.matrix[2][1] = 6.0;
 
     mat2.matrix[0][0] = 1.0;
-    mat2.matrix[0][1] = 2.0;
-    mat2.matrix[1][0] = 3.0;
-    mat2.matrix[1][1] = 4.0;
+    mat2.matrix[0][1] = -1.0;
+    mat2.matrix[0][2] = 1.0;
+    mat2.matrix[1][0] = 2.0;
+    mat2.matrix[1][1] = 3.0;
+    mat2.matrix[1][2] = 4.0;
 
-    s21_mult_matrix(&mat,mat2,&result);
-
-    printf("result:%f\n", result.matrix[1][1]);
+    s21_mult_matrix(&mat,&mat2,&result);
+    
+    printf("result:%f\n", result.matrix[2][2]);
 
     return 0;
 }
--- s21_mult_matrix.c
+++ s21_mult_matrix.c
@@ -12,13 +12,20 @@ int s21_mult_matrix(s21_matrix_t *A, s21_matrix_t *B, s21_matrix_t *result){
     if(A->rows < 1 || A->columns < 1 || B->rows < 1 || B->columns < 1){
         return INCORRECT_MATRIX;//1
     }
+    if(A->columns != B->rows){
+        return CALCULATION_ERROR;//2
+    }
 
     int status = s21_create_matrix(A->rows, B->columns, result);
+
 
     if(status == OK){
         for(int i = 0; i < A->rows; i++){
             for(int j = 0; j < B->columns; j++){
-                result->matrix[i][j] = 
+                result->matrix[i][j] = 0;
+                for(int k = 0; k < A->columns; k++){
+                    result->matrix[i][j] += A->matrix[i][k] * B->matrix[k][j];
+                }       
             }
         }
     }
