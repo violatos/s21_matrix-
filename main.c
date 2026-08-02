56144086d2ad9801b5bc147d970696fbb7e1395f
violatos <coffee2006cat@gmail.com>
2026-08-02 15:26:43 +0300

    finish add: s21_determinan.c

--- main.c
+++ main.c
@@ -24,16 +24,16 @@ int main(){
     mat.matrix[0][3] = 4.0;
     mat.matrix[1][0] = 5.0;
     mat.matrix[1][1] = 6.0;
-    mat.matrix[1][2] = 4.0;
-    mat.matrix[1][3] = 8.0;
-    mat.matrix[2][0] = 92.0;
-    mat.matrix[2][1] = 10.0;
-    mat.matrix[2][2] = 11.0;
-    mat.matrix[2][3] = 12.0;
-    mat.matrix[3][0] = 13.0;
-    mat.matrix[3][1] = 14.0;
-    mat.matrix[3][2] = 19.0;
-    mat.matrix[3][3] = 16.0;
+    mat.matrix[1][2] = 1.0;
+    mat.matrix[1][3] = 2.0;
+    mat.matrix[2][0] = 3.0;
+    mat.matrix[2][1] = 4.0;
+    mat.matrix[2][2] = 5.0;
+    mat.matrix[2][3] = 6.0;
+    mat.matrix[3][0] = 1.0;
+    mat.matrix[3][1] = 2.0;
+    mat.matrix[3][2] = 3.0;
+    mat.matrix[3][3] = 5.0;
 
 
     int status = s21_determinant(&mat, &det);
